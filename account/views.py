3819edb00337fa5d4628e201279d67779a04d98e
rakib114 <mahmud17209@gmail.com>
2024-03-10 16:25:42 +0600

    confirmlink added

--- account/views.py
+++ account/views.py
@@ -23,7 +23,7 @@ def SignUpView(request):
             user = form.save()
             token = default_token_generator.make_token(user)
             uid = urlsafe_base64_encode(force_bytes(user.pk))
-            confirm_link = f"http://127.0.0.1:8000/account/active/{uid}/{token}"
+            confirm_link = f"https://trainline-hero-1.onrender.com/account/active/{uid}/{token}"
             email_subject = "Confirm Your Email"
             email_body = render_to_string('confirm_email.html',{'confirm_link': confirm_link})
             email = EmailMultiAlternatives(email_subject, '', to=[user.email])
